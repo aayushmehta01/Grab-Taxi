91ad644b8247489aab879e2c586e51f5ce4528ba
aayushmehta01 <aayush2511m@gmail.com>
2025-03-02 12:58:40 +0530

    register user completed

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,22 @@
 const dontenv = require('dotenv');
 dontenv.config();
-
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const connectDB = require('./db/db')
+const userRoutes = require('./routes/user.routes');
+
+connectDB();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.get('/', (req, res)=>{
     res.send('Aayush here');
 });
+
+app.use('/api/users', userRoutes);
+
 
 module.exports = app;
